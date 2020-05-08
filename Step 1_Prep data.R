50d862a4dfe1a45cc391f22c789f4137a7b90495
Josh Cullen <joshcullen@Joshs-MacBook-Pro-3.local>
2020-05-08 16:19:43 -0400

    Make revisions to accomodate multiple IDs during covariate extraction

--- Step 1_Prep data.R
+++ Step 1_Prep data.R
@@ -14,7 +14,7 @@ library(sp)
 
 dat<- read.csv("three_banded_over20_for_Josh.csv", header = T, sep = ",")
 
-dat$id<- as.factor(dat$id)
+dat$id<- as.character(dat$id)
 dat$date<- as.POSIXct(strptime(dat$date, format = "%d/%m/%Y %H:%M"))
 coords<- dat[c("x","y")]
 dat.spdf<- SpatialPointsDataFrame(coords = coords, data = dat)
@@ -33,39 +33,73 @@ plot(classes_DL_padrao.tif)
 plot(EucDist_cerc_Copy.tif); points(dat.N$x, dat.N$y)
 plot(EucDist_cercaAm.tif); points(dat.S$x, dat.S$y)
 
-plot(EucDist_cerc_Copy.tif); points(dat.N$x, dat.N$y)
-dat.N %>% group_by(id) %>% tally()
-## Stick with tm14 from northern site using EucDist_cerc_Copy.tif layer
+#Need to project rasters same as tracks
+EucDist_cerc_Copy.tif<- projectRaster(EucDist_cerc_Copy.tif, crs = "+init=epsg:32721")
+crs(EucDist_cercaAm.tif)<- crs(EucDist_cerc_Copy.tif)
 
 
 
-tm14<- dat.N %>% filter(id == "tm14")
-tm14.line<- tm14 %>%
-  dplyr::select(x,y) %>% 
-  as.matrix() %>% 
-  st_linestring() %>% 
-  st_sfc(crs = projection(EucDist_cerc_Copy.tif)) %>% 
-  st_sf()
+# Convert DFs to linestrings by ID
+dat.N_sf<- st_as_sf(dat.N, coords=c("x","y"), crs = "+init=epsg:32721") %>% 
+  group_by(id) %>% 
+  summarize(do_union = FALSE) %>% 
+  st_cast("LINESTRING")
+plot(dat.N_sf)
 
-#Change res of EucDist layer closer to 30 m (26 m) fomr 13 m
+dat.S_sf<- st_as_sf(dat.S, coords=c("x","y"), crs = "+init=epsg:32721") %>% 
+  group_by(id) %>% 
+  summarize(do_union = FALSE) %>% 
+  st_cast("LINESTRING")
+plot(dat.S_sf)
+
+
+## Aggregate spatial data to same scale (which will likely be 30m)
+res(EucDist_cerc_Copy.tif) #13m 13m
 dist2rdN_30m<- raster::aggregate(EucDist_cerc_Copy.tif,
                                  fact = 2,
                                  fun = mean)
 
+res(EucDist_cercaAm.tif) #1m 1m
+dist2rdS_30m<- raster::aggregate(EucDist_cercaAm.tif,
+                                 fact = 30,
+                                 fun = mean)
 
-path<- raster::extract(dist2rdN_30m, tm14.line, along = TRUE, cellnumbers = TRUE)
-path_df = purrr::map_dfr(path, as_data_frame, .id = "ID")
-names(path_df)[3]<- "dist2rd"
-path_coords = xyFromCell(dist2rdN_30m, path_df$cell)
+
+## Extract values from raster layer for each track
+
+#N
+path_N<- raster::extract(dist2rdN_30m, dat.N_sf, along = TRUE, cellnumbers = TRUE)
+path_N_df = purrr::map_dfr(path_N, as_data_frame, .id = "id")
+names(path_N_df)[3]<- "dist2rd"
+path_N_coords = xyFromCell(dist2rdN_30m, path_N_df$cell)
 # pair_dist = geosphere::distGeo(path_coords)[-nrow(path_coords)]
 # transect_df$dist = c(0, cumsum(pair_dist)) 
 
 #double-check that extracted grid cells overlap with tm14 path
 bar<- dist2rdN_30m
 bar[]<- NA
-bar[path_df$cell]<- dist2rdN_30m[path_df$cell]
+bar[path_N_df$cell]<- dist2rdN_30m[path_N_df$cell]
 plot(bar)
-plot(st_geometry(tm14.line), add=T)
+plot(st_geometry(dat.N_sf), add=T)
+
+
+#S
+path_S<- raster::extract(dist2rdS_30m, dat.S_sf, along = TRUE, cellnumbers = TRUE)
+path_S_df = purrr::map_dfr(path_S, as_data_frame, .id = "id")
+names(path_S_df)[3]<- "dist2rd"
+path_S_coords = xyFromCell(dist2rdS_30m, path_S_df$cell)
+# pair_dist = geosphere::distGeo(path_coords)[-nrow(path_coords)]
+# transect_df$dist = c(0, cumsum(pair_dist)) 
+
+#double-check that extracted grid cells overlap with tm14 path
+bar<- dist2rdS_30m
+bar[]<- NA
+bar[path_S_df$cell]<- dist2rdS_30m[path_S_df$cell]
+plot(bar)
+plot(st_geometry(dat.S_sf), add=T)
+
+
+
 
 #Label all observations with cell number and time interval
 tm14$cell<- cellFromXY(dist2rdN_30m, tm14[,c("x","y")])
