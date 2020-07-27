8d698f0b3078fa1796f81b5f0b6b9f1b90fe3469
drvalle1 <drvalle1@users.noreply.github.com>
2020-07-27 12:29:56 -0400

    Commit 16
    I removed the latent groups. This code models landscape resistance, regardless of animal behavior. If people believe that behavior can influence this resistance, they can just subset their data to the relevant behavior specific pieces.

--- compare estim and true.R
+++ compare estim and true.R
@@ -1,7 +1,6 @@
 store.llk=mod.res$llk
 store.b=mod.res$b.gamma
 store.betas=mod.res$betas
-z.estim=mod.res$z.estim
 store.theta=mod.res$theta
 
 #look at correlation
@@ -12,35 +11,23 @@ k
 #look at overall convergence
 par(mfrow=c(1,1))
 plot(store.llk,type='l')
-nburn=400
+nburn=500
 abline(v=nburn,col='red')
 plot(store.llk[nburn:ngibbs],type='l')
 
 #look at convergence betas
-par(mfrow=c(3,3),mar=rep(1,4))
-ind=sample(1:ncol(store.betas),size=9)
-for (i in 1:9) plot(store.betas[1:ngibbs,ind[i]],type='l')
-for (i in 1:9) plot(store.betas[nburn:ngibbs,ind[i]],type='l')
+par(mfrow=c(2,2),mar=rep(1,4))
+for (i in 1:4) plot(store.betas[1:ngibbs,i],type='l')
+for (i in 1:4) plot(store.betas[nburn:ngibbs,i],type='l')
 
 #look at convergence of b.gammas
 par(mfrow=c(1,1))
 plot(store.b,type='l')
 abline(h=b.true,col='red')
 
-#look at z's
-fim=data.frame(z.estim=z.estim,z.true=z.true)
-tab1=table(fim); tab1
-ordem=numeric()
-for (i in 1:ncol(tab1)){
-  ind=which(tab1[,i]==max(tab1[,i]))
-  ordem=c(ordem,ind)
-}
-tab1[ordem,]
-
 #look at betas
-ngroup=4
 par(mfrow=c(1,1),mar=rep(3,4))
-betas.estim=matrix(store.betas[ngibbs,],ncol(store.betas)/ngroup,ngroup)
-rango=range(c(betas.estim[,ordem],betas.true))
-plot(betas.true,betas.estim[,ordem],xlim=rango,ylim=rango)
+betas.estim=store.betas[ngibbs,]
+rango=range(c(betas.estim,betas.true))
+plot(betas.true,betas.estim,xlim=rango,ylim=rango)
 lines(rango,rango,col='red')
--- gibbs_resist_func.R
+++ gibbs_resist_func.R
@@ -1,93 +1,75 @@
-sample.betas=function(betas,xmat,ysoma,jump,nparam,b.gamma,var.betas,seg.id,z,ngroup,nagg){
+sample.betas=function(betas,xmat,ysoma,jump,nparam,b.gamma,var.betas,seg.id,nagg){
   betas.old=betas.new=betas
-  betas.prop=matrix(rnorm(nparam*ngroup,mean=betas,sd=jump),nparam,ngroup)
-  accept=matrix(0,nparam,ngroup)
+  betas.prop=matrix(rnorm(nparam,mean=betas,sd=jump),nparam,1)
+  accept=matrix(0,nparam,1)
 
   for (i in 1:nparam){
     betas.new=betas.old
     betas.new[i,]=betas.prop[i,]
     pold=get.llk(betas=betas.old,xmat=xmat,ysoma=ysoma,
-                 b.gamma=b.gamma,seg.id=seg.id,ngroup=ngroup,nagg=nagg)
+                 b.gamma=b.gamma,seg.id=seg.id,nagg=nagg)
     pnew=get.llk(betas=betas.new,xmat=xmat,ysoma=ysoma,
-                 b.gamma=b.gamma,seg.id=seg.id,ngroup=ngroup,nagg=nagg)
+                 b.gamma=b.gamma,seg.id=seg.id,nagg=nagg)
 
     #get priors
     prior.old =dnorm(betas.old[i,] ,mean=0,sd=sqrt(var.betas[i]),log=T)
     prior.prop=dnorm(betas.prop[i,],mean=0,sd=sqrt(var.betas[i]),log=T)
 
-    #sum the loglikel for the correct group
-    pold1=GetSomaLlkGroups(llk=pold, z=z-1, ngroups=ngroup)
-    pnew1=GetSomaLlkGroups(llk=pnew, z=z-1, ngroups=ngroup)
+    #sum the loglikel
+    pold1=sum(pold)
+    pnew1=sum(pnew)
     
     #MH algorithm
     pold2=pold1+prior.old
     pnew2=pnew1+prior.prop
     pthresh=exp(pnew2-pold2)
-    cond=runif(ngroup)<pthresh
-    betas.old[i,cond]=betas.new[i,cond]
-    accept[i,cond]=1
+    cond=runif(1)<pthresh
+    betas.old[i]=ifelse(cond,betas.new[i],betas.old[i])
+    accept[i]=1
   }
   
   list(accept=accept,betas=betas.old)
 }
 #--------------------------------------------
 sample.betas.joint=function(betas,xmat,ysoma,nparam,b.gamma,var.betas,seg.id,
-                            z,ngroup,nagg,var1){
+                            nagg,var1){
   betas.old=betas.new=betas
-  for (i in 1:ngroup){
-    betas.new[,i]=rmvnorm(1,mean=betas.old[,i],sigma=var1[[i]])
-  }
+  betas.new=t(rmvnorm(1,mean=betas.old,sigma=var1))
 
   pold=get.llk(betas=betas.old,xmat=xmat,ysoma=ysoma,
-               b.gamma=b.gamma,seg.id=seg.id,ngroup=ngroup,nagg=nagg)
+               b.gamma=b.gamma,seg.id=seg.id,nagg=nagg)
   pnew=get.llk(betas=betas.new,xmat=xmat,ysoma=ysoma,
-               b.gamma=b.gamma,seg.id=seg.id,ngroup=ngroup,nagg=nagg)
+               b.gamma=b.gamma,seg.id=seg.id,nagg=nagg)
     
   #get priors
-  var.betas1=matrix(var.betas,nparam,ngroup)
-  prior.old =apply(dnorm(betas.old,mean=0,sd=sqrt(var.betas1),log=T),2,sum)
-  prior.new=apply(dnorm(betas.new,mean=0,sd=sqrt(var.betas1),log=T),2,sum)
+  prior.old =apply(dnorm(betas.old,mean=0,sd=sqrt(var.betas),log=T),2,sum)
+  prior.new=apply(dnorm(betas.new,mean=0,sd=sqrt(var.betas),log=T),2,sum)
     
-  #sum the loglikel for the correct group
-  pold1=GetSomaLlkGroups(llk=pold, z=z-1, ngroups=ngroup)
-  pnew1=GetSomaLlkGroups(llk=pnew, z=z-1, ngroups=ngroup)
+  #sum the loglikel
+  pold1=sum(pold)
+  pnew1=sum(pnew)
     
   #MH algorithm
   pold2=pold1+prior.old
   pnew2=pnew1+prior.new
   pthresh=exp(pnew2-pold2)
-  cond=runif(ngroup)<pthresh
-  betas.old[,cond]=betas.new[,cond]
-
-  accept=rep(0,ngroup)
-  accept[cond]=1
+  cond=runif(1)<pthresh
+  accept=0
+  if (cond) {
+    betas.old=betas.new
+    accept=1
+  }
 
   list(accept=accept,betas=betas.old)
 }
 #--------------------------------------------
-get.llk=function(betas,xmat,ysoma,b.gamma,seg.id,nagg,ngroup){
+get.llk=function(betas,xmat,ysoma,b.gamma,seg.id,nagg){
   media=exp(xmat%*%betas)
-  soma.media1=GetSomaMediaAllGroups(media=media, ngroups=ngroup, nysoma=nagg,SegID=seg.id-1)
+  soma.media1=GetSomaMediaOneGroup(media=media, nagg=nagg,SegID=seg.id-1)
   a.gamma1=b.gamma*soma.media1
-  ysoma.mat=matrix(ysoma,nagg,ngroup)
-  dgamma(ysoma.mat,a.gamma1,b.gamma,log=T)
+  dgamma(ysoma,a.gamma1,b.gamma,log=T)
 }
 #--------------------------------------------------
-sample.z=function(betas,xmat,ysoma,b.gamma,seg.id,ngroup,nagg,theta){
-  llk=get.llk(betas=betas,xmat=xmat,ysoma=ysoma,b.gamma=b.gamma,
-              seg.id=seg.id,ngroup=ngroup,nagg=nagg)
-  ltheta=matrix(log(theta),nagg,ngroup,byrow=T)
-  
-  llk=llk+ltheta
-  prob=GetProbZ(llk=llk)
-  # max1=apply(llk,1,max)
-  # llk1=llk-max1
-  # llk2=exp(llk1)
-  # llk3=llk2/apply(llk2,1,sum)
-  tmp=rmultinom1(prob=prob, runif1=runif(nrow(prob)))
-  tmp+1
-}
-#----------------------------------
 print.adapt = function(accept1z,jump1z,accept.output){
   accept1=accept1z; jump1=jump1z; 
 
