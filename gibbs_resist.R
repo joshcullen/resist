212d8490aa7d3164f87f8e678d9a38a1277e871f
Josh Cullen <joshcullen10@gmail.com>
2020-07-27 11:42:06 -0400

    Update gibbs_resist.R to match 'master' branch

--- gibbs_resist.R
+++ gibbs_resist.R
@@ -1,77 +1,80 @@
-gibbs_resist=function(ysoma,xmat,seg.id,ngroup,ngibbs,nburn,gamma1,var.betas){
+gibbs_resist=function(ysoma,xmat,seg.id,ngroup,ngibbs,nburn,gamma1,var.betas,w,MaxIter){
   n=nrow(xmat)  
   nparam=ncol(xmat)
   nagg=length(ysoma)  
   
   #initial parameters
   betas=matrix(0,nparam,ngroup)
-  b.gamma=2
+  b.gamma=1
   z=sample(1:ngroup,size=nagg,replace=T)
   theta=rep(1/ngroup,ngroup)
+  tmp=diag(1,nparam)
+  
+  #for joint sampling of betas
+  var1=list()
+  for (jj in 1:ngroup){
+    var1[[jj]]=tmp
+  }
+  ind.betas=matrix(1:(nparam*ngroup),nparam,ngroup)
   
   #stuff for gibbs sampler
-  jump1=list(betas=matrix(0.1,nparam,ngroup),b.gamma=0.1)
-  accept1=list(betas=matrix(0,nparam,ngroup),b.gamma=0)
+  jump1=list(betas=matrix(0.1,nparam,ngroup),betas.joint=rep(1,ngroup))
+  accept1=list(betas=matrix(0,nparam,ngroup),betas.joint=rep(0,ngroup))
+  accept.output=50  
   store.betas=matrix(NA,ngibbs,nparam*ngroup)
   store.b=matrix(NA,ngibbs,1)
   store.theta=matrix(NA,ngibbs,ngroup)
   store.llk=matrix(NA,ngibbs,1)
-  accept.output=50
   
   for (i in 1:ngibbs){
     print(i)
     
-    #sample betas
+    #sample betas    
     tmp=sample.betas(betas=betas,xmat=xmat,ysoma=ysoma,jump=jump1$betas,
                      b.gamma=b.gamma,nparam=nparam,var.betas=var.betas,
                      seg.id=seg.id,ngroup=ngroup,nagg=nagg,z=z)
     betas=tmp$betas
     accept1$betas=accept1$betas+tmp$accept
-
+    
+    tmp=sample.betas.joint(betas=betas,xmat=xmat,ysoma=ysoma,
+                           b.gamma=b.gamma,nparam=nparam,var.betas=var.betas,
+                           seg.id=seg.id,ngroup=ngroup,nagg=nagg,z=z,var1=var1)
+    betas=tmp$betas
+    accept1$betas.joint=accept1$betas.joint+tmp$accept
+    
+    # betas=betas.true
+    
     #sample b.gamma
-    tmp=sample.b.gamma(betas=betas,xmat=xmat,ysoma=ysoma,jump=jump1$b.gamma,
-                       b.gamma=b.gamma,seg.id=seg.id,ngroup=ngroup,nagg=nagg,z=z)
-    b.gamma=tmp$b.gamma
-    accept1$b.gamma=accept1$b.gamma+tmp$accept
+    b.gamma=Sample_bgamma(ngroups=ngroup,nparam=nparam,xmat=xmat,
+                          z=z,ysoma=ysoma,betas=betas,b.gamma=b.gamma,
+                          w=w,MaxIter=100,seg.id=seg.id,nagg=nagg)
     # b.gamma=b.true
     
     #sample z
     z=sample.z(betas=betas,xmat=xmat,ysoma=ysoma,b.gamma=b.gamma,
                seg.id=seg.id,ngroup=ngroup,nagg=nagg,theta=theta)
+    # z=z.true
     
     #sample theta
-    theta=sample.theta(z=z,gamma1=gamma1,ngroup=ngroup)
+    theta=rep(1/ngroup,ngroup)
     
     #get llk
     p=get.llk(betas=betas,xmat=xmat,ysoma=ysoma,b.gamma=b.gamma,
               seg.id=seg.id,ngroup=ngroup,nagg=nagg)
     #sum the loglikel for the correct group
     llk1=GetSomaLlkGroups(llk=p, z=z-1, ngroups=ngroup)
-    # llk=0
-    # for (j in 1:ngroup){
-    #   cond=z==j
-    #   llk=llk+sum(p[cond,j])
-    # }    
-    
-    #re-order groups from time to time
-    if (i<nburn & i%%accept.output==0){
-      ind=order(theta,decreasing=T)
-      theta=theta[ind]
-      betas=betas[,ind]
-      jump1$betas=jump1$betas[,ind]
-      z1=rep(NA,length(z))
-      for (k in 1:ngroup){
-        cond=z==ind[k]
-        z1[cond]=k
-      }
-      z=z1
-    }
     
     #adaptation MH algorithm
     if (i<nburn & i%%accept.output==0){
       k=print.adapt(accept1z=accept1,jump1z=jump1,accept.output=accept.output)
       accept1=k$accept1
       jump1=k$jump1
+      
+      #get correlation structure from posterior samples
+      for (jj in 1:ngroup){
+        seq1=(i-accept.output+1):(i-1)
+        var1[[jj]]=var(store.betas[seq1,ind.betas[,jj]])  
+      }
     }
     
     #store results
